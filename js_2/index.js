4e95a491fbf42601d043ce79f15564fabb70c0e5
ros.skyba <rostyslav.skyba@eleks.com>
2022-02-03 21:51:08 +0200

    Update: JS_2

---
+++ js_2/index.js
@@ -0,0 +1,72 @@
+const AM_I_ADULT = true;
+const IS_FRIDAY = false;
+
+let my_age = 27; // number
+let num = 10n; // Big Int
+
+// console.log(-0);
+// console.log(Infinity); //  
+// console.log(-Infinity); // 5/0
+
+// console.log(NaN); // Not a Number
+
+// const MY_NAME = `
+// Ros is ${my_age} yo
+// And he is FE teach
+// at Beetroot
+// `; // Template string;
+
+// console.log('Is friday today?' + IS_FRIDAY + " Ohh. That's a pitty") // default concat strings
+
+// console.log(MY_NAME);
+
+let mySum; // 
+
+let money = 100;
+//
+// ...
+//
+
+money = 10;
+100 < 10 ? console.log('Yes') : console.log('No');
+
+let calc = 5;
+let calc2 = 100
+console.log(calc++);  // unary increment
+console.log(++calc2); // 101 
+
+
+// Binary
+// 5 + 5; // 10;
+// 5 - 5; // 0
+// 5 / 5; 0 
+
+// myname + surname
+
+let today_date = '3.02' 
+
+// 5 < 10; 100 > 101; 100 >= 100
+
+// let x = 100;
+// x += 10; // 110;
+
+// const y = 10;
+// y *=10; 
+
+const my_new_num = Number(10);
+const my_new_str = String('Ros');
+
+const result = 'hello' + 10 + 10 + 10; // 'hello' + '10' === 'hello101010'
+
+let xy = undefined;
+let yx = null;
+
+console.log(xy == yx); // 
+console.log(xy === yx);
+
+
+console.log('5 != 5', 5 != '5') 
+console.log('5 !== 5', 5 !== '5') 
+
+Number('100');
++'100';

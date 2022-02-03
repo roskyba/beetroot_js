9ee1057c4d57c09ec924e247ddb666aa050bc69f
ros.skyba <rostyslav.skyba@eleks.com>
2022-02-03 18:46:48 +0200

    Update: JS_1

---
+++ js_1/script/index.js
@@ -0,0 +1,40 @@
+/* 
+тут мій коментар
+тут я написав простий код
+console.log('Hello')
+ ------
+*/
+
+
+
+// 5 + 5 = 10
+// let myName = 'Ros'
+// let me = '4 Ros' // $, _ @
+// let _myHomeTown = 'Lviv';
+// let $country = 'UA';
+// var myAge = 27;
+const PI_NUMBER = 3.14;
+const SUN_AROUND_EARTH = false;
+const SUN; // must be initialised
+// user_name != user_Name
+
+let userName = prompt('Хто ти?');
+let userAge = prompt('Cкільки тобі років?');
+let userIsOk = prompt('Чи тобі ок?');
+
+let number1 = 10;
+
+let result = 5 + 5;  // 10 expression
+
+let number2 = 20; // statement in hJS
+let number3 = -3;
+
+console.log('Result = ', result)
+
+let dobNumbers = number1 * number2;
+
+let newSum = dobNumbers + number3;  
+
+console.log('Користувач успішно залогінений, під іменем - ', userName);
+alert('HELP!!!!');
+

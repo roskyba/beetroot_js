7c8e456d5f50e673db08f9bc4e8ef2b2e2dbcc50
ros.skyba <rostyslav.skyba@eleks.com>
2022-02-15 21:29:03 +0200

    Update: JS_7

---
+++ js_7/index.js
@@ -0,0 +1,84 @@
+[1, 2, 3, 4, 5, 7, 8].reduce((previousValue, currentValue) => {
+  console.log("previousValue ===", previousValue);
+  console.log("currentValue ===", currentValue);
+  return previousValue + currentValue;
+});
+
+function addNumbers(num1, num2, num3) {}
+
+const res = addNumbers(fromBE); // ->> undefined
+
+
+fromBE - 200; // if ( resut < 200 )
+
+// function init() {
+// const mainAdd = () => {}..
+// const reducer = () =>
+// [1,2,4].reduceRight()
+// }
+
+function calculator() {
+  console.log("outer");
+  let innerValue = 0;
+  return function () {
+    innerValue++;
+    console.log("innerValue ==", innerValue);
+  };
+}
+
+
+const calc1 = calculator();
+const calc2 = calculator();
+const calc3 = calculator();
+const calc4 = calculator();
+const calc5 = calculator();
+
+1 - calculator(); calc5() // this
+2 - obj.drive(); // this 
+3 - .map(callback) // this
+
+
+
+function logThis() {
+    console.log('this ==', this)
+}
+
+
+const logThis2 = function() { // this === {}   console.log('this ==', this)}
+
+
+
+const obj = {
+    method: logThis,
+
+};
+
+
+logThis(); // ---> this === Window object
+obj.method(); // ----> this === obj
+[1,2,4].map(logThis); // ----> this === Window object
+
+
+
+const obj2 = {
+    method: logThis,
+    method2: () => {
+        console.log('this ===', this);
+    }
+};
+
+logThis.call({name: 'Ros', age: 27}, )
+
+const printName = (userName, userAge) => {
+    console.log(this.userName + this.userAge);
+};
+
+printName('Ros', 27);
+
+printName.call({name: 'Oleh', city: 'IF'}, 'Maria', 20);
+
+function log() {
+    console.log(`${this.userName} is ${this.userAge} yo`);
+};
+
+log.call({userAge: 20, userName: 'Oleh'});

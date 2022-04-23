ab21d95e881349ccd05488969472e2ac9ed6bdcb
ros.skyba <rostyslav.skyba@eleks.com>
2022-04-23 13:11:36 +0300

    Update: JS_14 practice; part 1

---
+++ js_14/index.js
@@ -0,0 +1,33 @@
+// console.log('Initialize fetch request... Stand by....')
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     .then(function callback(response) {
+//         return response.json();
+//     })
+//     .then((res) => {
+//         console.log('Res =', res)
+//         console.log(res.title)
+//     })
+
+const getData = document.getElementById('btn');
+
+const sendHTTPRequest = () => {
+    // This function will hanlde sending HTTP request to BackEnd
+    console.log('Sending HTTP request...');
+
+    fetch('https://reqres.in/api/users')
+        .then(r => r.json())
+        .then(data => {
+            console.log('Data =', data)
+            console.log('We finished fetching. Data is available')
+        })
+        .catch(error => {
+            console.log('ERROR =', error)
+        });
+}
+
+getData.addEventListener('click', () => {
+    console.log('We will get some data from server')
+    // 
+    sendHTTPRequest(); //
+})

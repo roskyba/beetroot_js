b0dddf8623c7e53994c2cf0cab34b0dcf60dd8ae
ros.skyba <rostyslav.skyba@eleks.com>
2022-04-23 20:29:18 +0300

    Update: JS_13 practice; part 2

---
+++ js_13/index.js
@@ -0,0 +1,12 @@
+
+
+$(document).ready(function () {
+    $('.your-class').slick({
+        // adaptiveHeight: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        speed: 1000
+    });
+});

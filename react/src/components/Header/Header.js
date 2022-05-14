23696acb84eb2b8fd53134288683c6243733fb38
ros.skyba <rostyslav.skyba@eleks.com>
2022-05-14 14:34:32 +0300

    React 2

---
+++ react/src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import './Header.css'
+
+function Header() {
+
+    return (
+        <>
+            <header className="header">
+                <h2>
+                    I am the Header
+                </h2>
+            </header>
+        </>
+    )
+}
+
+export default Header
